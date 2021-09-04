ad886b0978d5679853b111ada24b543a4be3cc5b
Dienpham <dienpham187294@gmail.com>
2021-09-04 10:07:33 +0700

    add

---
+++ pages/index.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import useSocket from "../hooks/useSocket";
+
+export default function Blah() {
+	const [messages, setMessages] = useState([]);
+	const [message, setMessage] = useState([]);
+	const socket = useSocket();
+
+	useEffect(() => {
+		if (socket) {
+			socket.on("message.chat1", message => {
+				console.log(message)
+			});
+		}
+	}, [socket]);
+
+	function submit(e) {
+		e.preventDefault();
+		socket &&
+			socket.emit("message.chat1", {
+				id: socket.id,
+				value: message
+			});
+	}
+
+	return (
+		<div>
+			<form onSubmit={submit}>
+				<input
+					value={message}
+					onChange={e => setMessage(e.target.value)}
+				/>
+				<button>submit</button>
+			</form>
+			<ul>
+				{messages.map(msg => (
+					<li key={msg.id}>{msg.value}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
